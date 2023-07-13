e9339699ff87cb0b6e1b5922d540bed6aa995c09
26YK <a0548475725@gmail.com>
2023-07-13 11:40:59 +0300

    update table data

--- app/types/tables.js
+++ app/types/tables.js
@@ -6,7 +6,28 @@ export const tables = [
             {key: 'id', value: 'תעודת זהות'},
             {key: 'firstName', value: 'שם פרטי'},
             {key: 'lastName', value: 'שם משפחה'},
-            {key: 'phone', value: 'טלפון'}
+            {key: 'phone', value: 'טלפון נייד'},
+            {key: 'address', value: 'רחוב'},
+            {key: 'numberAddress', value: 'מס רחוב'},
+            {key: 'city', value: 'ישוב'},
+            {key: 'kolel', value: 'חופשי1'},
+            {key: 'group', value: 'חופשי3'},
+            {key: 'matrimCode', value: 'מתרים'},
+            {key: 'isTorem', value: 'סיווג תורם'},
+            {key: 'tz', value: 'תז'},
+            {key: 'contry', value: 'ארץ'},
+            {key: 'paymentType', value: 'חופשי11'},
+            {key: 'beforeTitle', value: 'תואר לפני'},
+            {key: 'afterTitle', value: 'תואר אחרי'},
+            {key: 'krayera', value: 'עיסוק'},
+            {key: 'notes', value: 'הערות'},
+            {key: 'personStatus', value: 'סטטוס'},
+            {key: 'email', value: 'אימייל'},
+            {key: 'chatan', value: 'חופשי'},
+            {key: 'fathzerName', value: 'שם_אב'},
+            {key: 'women', value: 'שם_ב_זוג'},
+            {key: 'isMatrim', value: 'סיווג מתרים'}
+
         ]
     }
 ]
